9d4f2b68bca7e4d2168d3bf0808e7ac0b6462427
hg431 <126284856+hg431@users.noreply.github.com>
2023-03-02 20:09:12 +0100

    CSS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
 // Addition
 
-// Subtraction
+const add = function (a, b) {
+    return a + b;
+  };
+  
+// Subtract
 
-// Multiplication
+const subtract = function (a, b) {
+    return a - b;
+  };
+
+// Multiply
+
+const multiply = function (array) {
+    return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
+  };
 
 // Division
 
+const divide = function (a, b) {
+    return a / b;
+};
+
 // Operate
+
+function operate(a, b, function) {
+    if function == "add"
+}
