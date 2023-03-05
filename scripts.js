7e1a7a7d95ce7ce1221d324c0363e5c59f8fadc1
hg431 <126284856+hg431@users.noreply.github.com>
2023-03-05 15:15:12 +0100

    Test

--- scripts.js
+++ scripts.js
@@ -45,34 +45,82 @@ let displayValue = "0";
 
 // Add event listeners to numbers 0-9
 
-var i = 0;
-while (i < 10) {
-  document.getElementById(i).addEventListener("click", function() {
-  buttonClick(this)
+document.getElementById("0").addEventListener("click", function() {
+  buttonClick("0")
   });
-  i++;
-}
 
-// Operators
+document.getElementById("1").addEventListener("click", function() {
+  buttonClick("1")
+  });
+    
+document.getElementById("2").addEventListener("click", function() {
+buttonClick("2")
+});
 
-const divideClick = document.getElementById("divide");
-divideClick.addEventListener("click", function() {
+document.getElementById("3").addEventListener("click", function() {
+buttonClick("3")
+});
+
+document.getElementById("4").addEventListener("click", function() {
+buttonClick("4")
+});
+
+document.getElementById("4").addEventListener("click", function() {
+buttonClick("4")
+});
+
+document.getElementById("5").addEventListener("click", function() {
+buttonClick("5")
+});
+
+document.getElementById("6").addEventListener("click", function() {
+  buttonClick("6")
+});
+
+document.getElementById("7").addEventListener("click", function() {
+  buttonClick("7")
+});
+
+document.getElementById("8").addEventListener("click", function() {
+  buttonClick("8")
+});
+
+document.getElementById("9").addEventListener("click", function() {
+  buttonClick("9")
+});
+
+// Add event listeners for add, multiply, subtract, divide
+
+document.getElementById("add").addEventListener("click", function() {
+  buttonClick("add")
+});
+
+document.getElementById("multiply").addEventListener("click", function() {
+  buttonClick("multiply")
+});
+
+document.getElementById("subtract").addEventListener("click", function() {
+  buttonClick("subtract")
+});
+
+document.getElementById("divide").addEventListener("click", function() {
   buttonClick("divide")
 });
 
-// Repeat for all operators
 
 const screen = document.getElementById("screen");
 
-const clearClick = document.getElementById("clear");
-clearClick.addEventListener("click", function() {
+document.getElementById("clear").addEventListener("click", function() {
   displayValue = "0";
   screen.textContent = "0";
 });
 
-
 function buttonClick(e) {
-  screen.textContent = "buttonClick " + e;
+  if (e == 0 || e == 1 || e == 2 || e == 3 || e == 4 || e == 5 || e == 6 || e == 7 || e == 8 || e == 9) {
+    screen.textContent += e;
+  } else if (e == "add") {
+    screen.textContent += "&#43;";
+  }
 };
 
-document.getElementById("screen").textContent = "0";
+screen.textContent = "0";
