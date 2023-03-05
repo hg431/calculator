af75d4efa64192cb8264bc2cae240aedf4616230
hg431 <126284856+hg431@users.noreply.github.com>
2023-03-05 10:54:13 +0100

    Added debugging tools and operator function

--- scripts.js
+++ scripts.js
@@ -27,5 +27,23 @@ const divide = function (a, b) {
 // Operate
 
 function operate(a, b, function) {
-    if function == "add"
-}
+    if function == "add" {
+      return add(a,b);
+    } else if function == "subtract" {
+      return subtract(a,b);
+    } else if function == "multiply" {
+      return multiply(a,b) 
+    } else if function == "divide" {
+      return divide(a,b);
+    } else {
+      return "Function not specified";
+    };
+};
+
+// Debug
+
+document.getElementByID("debug").textContent += "Add 5 and 4 " + add(5,4) 
+                                              + "Subtract 5 from 4 " + subtract(4,5) 
+                                              + "Multiply 5 and 4 " + multiply(5,4) 
+                                              + "Divide 10 by 2 " + divide(10,2)
+                                              + "Operate function 10 add 2" + operate(10,2,"add");
